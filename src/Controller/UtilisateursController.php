b91fc7797925ec8deef5177d7d0847392ae9ae73
François BANITZ <francois.banitz@gmail.com>
2021-02-22 17:38:18 +0100

    affichage de la liste des utilisateurs

--- src/Controller/UtilisateursController.php
+++ src/Controller/UtilisateursController.php
@@ -2,19 +2,90 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\UserType;
+use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UtilisateursController extends AbstractController
 {
     /**
      * @Route("/admin/utilisateurs", name="admin_utilisateurs")
      */
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+        $users = $userRepository->findAll();
         return $this->render('admin/utilisateurs.html.twig', [
-            'controller_name' => 'AdminUtilisateursController',
+            'users' => $users,
         ]);
+    }
+
+    /**
+     * @Route("/admin/users/create", name="user_create")
+     */
+    public function createUser(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($user);
+            $manager->flush();
+            return $this->redirectToRoute('admin_users');
+       }
+        return $this->render('admin/userForm.html.twig', [
+            'userForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/users/update-{id}", name="user_update")
+     */
+    public function updateUser(UserRepository $userRepository, $id, Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = $userRepository->find($id);
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            if($form->get('plainPassword')->getData() !== null) {
+                $user->setPassword(
+                    $passwordEncoder->encodePassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                );
+            }
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($user);
+            $manager->flush();
+            return $this->redirectToRoute('admin_users');
+        }
+        return $this->render('admin/userForm.html.twig', [
+            'userForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/users/delete-{id}", name="user_delete")
+     */
+    public function deleteUser(UserRepository $userRepository, $id)
+    {
+        $user = $userRepository->find($id);
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($user);
+        $manager->flush();
+        return $this->redirectToRoute('admin_users');
     }
 }
