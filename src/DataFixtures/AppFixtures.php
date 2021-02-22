310c4fa610788da653d094ffebc9af80ce3e9e5b
MelvDouc <melv.douc@gmail.com>
2021-02-22 10:05:21 +0100

    fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Produits;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,15 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for($count = 0; $count < 10; $count++) {
+            $produit = new Produits();
+            $produit->setNom('produit n° '.$count);
+            $produit->setDescription('Description du produit #'.$count);
+            $produit->setImage(($count % 2 == 0) ? 'homme.jpg' : 'femme.jpg');
+            $produit->setPrix(random_int(1,200).'€');
+            $produit->setIdCategorie('1');
+            $produit->setIdTailles('1');
+        }
 
         $manager->flush();
     }
