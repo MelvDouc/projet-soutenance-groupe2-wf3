b24918cabbf6411b8090f7831287492752bc0a85
MatthieuSc <matthieu.scherer@me.com>
2021-02-24 12:35:45 +0100

    mise à jour CategoriesController et SousCategoriesController

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -10,13 +10,84 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class CategoriesController extends AbstractController
 {
     /**
-     * @Route("/categories", name="categories")
+     * @Route("/admin/categories", name="admin_categories")
      */
-    public function index(): Response
+    public function index(CategoriesRepository $categoriesRepository): Response
     {
-        return $this->render('categories/categories.html.twig', [
-            'controller_name' => 'CategoriesController',
+        $categories = $categoriesRepository->findAll();
+        return $this->render('admin/categories.html.twig', [
+            'categories' => $categories,
         ]);
+    }
+
+
+    /**
+     * @Route("/admin/categorie/create", name="categorie_create")
+     */
+    public function createCategorie(Request $request)
+    {
+        $categorie = new Categories();
+        $form = $this->createForm(CategoriesType::class, $categorie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+              
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($categorie);
+                $manager->flush();
+                $this->addFlash(
+                    'success',
+                    'La categorie a bien été ajoutée.'
+                );
+            } else {
+                $this->addFlash(
+                    'danger',
+                    'Une erreur est survenue lors de l\'ajout de la catégorie'
+                );
+
+            return $this->redirectToRoute('admin_categories');
+        }
+        return $this->render('admin/categoriesForm.html.twig', [
+            'categoriesForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/categories/update-{id}", name="categorie_update")
+     */
+    public function updateCategorie(CategoriesRepository $categoriesRepository, $id, Request $request)
+    {
+        $categorie = $categoriesRepository->find($id);
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($categorie);
+            $manager->flush();
+            $this->addFlash(
+                'success',
+                'Le categorie a bien été modifiée'
+            );
+            return $this->redirectToRoute('admin_categories');
+        }
+        return $this->render('admin/categorieForm.html.twig', [
+            'categorieForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/categorie/delete-{id}", name="categorie_delete")
+     */
+    public function deleteCategorie(CategoriesRepository $categoriesRepository, $id)
+    {
+        $categorie = $CategoriesRepository->find($id);
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($categorie);
+        $manager->flush();
+        $this->addFlash(
+            'success',
+            'Le categorie a bien été supprimé'
+        );
+        return $this->redirectToRoute('admin_categories');
     }
 
 }
--- src/Controller/SousCategoriesController.php
+++ src/Controller/SousCategoriesController.php
@@ -2,19 +2,91 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\SousCategories;
+use App\Repository\SousCategoriesRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SousCategoriesController extends AbstractController
 {
     /**
-     * @Route("/sous/categories", name="sous_categories")
+     * @Route("/admin/sous-categories", name="admin_sous_categories")
      */
-    public function index(): Response
+    public function index(SousCategoriesRepository $souscategoriesRepository): Response
     {
+        $sousCategorie = $souscategoriesRepository->findAll();
         return $this->render('sous_categories/index.html.twig', [
-            'controller_name' => 'SousCategoriesController',
+            'sousCategorie' => $SousCategories
         ]);
+    }
+
+    /**
+     * @Route("/admin/sous-categorie/create", name="sous_categorie_create")
+     */
+    public function createSousCategorie(Request $request)
+    {
+        $sousCategorie = new SousCategories();
+        $form = $this->createForm(SousCategoriesType::class, $sousCategorie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+              
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($sousCategorie);
+                $manager->flush();
+                $this->addFlash(
+                    'success',
+                    'La sous-catégorie a bien été ajoutée.'
+                );
+            } else {
+                $this->addFlash(
+                    'danger',
+                    'Une erreur est survenue lors de l\'ajout de la sous-catégorie'
+                );
+
+            return $this->redirectToRoute('admin_sous_categories');
+        }
+        return $this->render('admin/sousCategoriesForm.html.twig', [
+            'sousCategoriesForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/sous-categorie/update-{id}", name="sous_categorie_update")
+     */
+    public function updateSousCategorie(SousCategoriesRepository $souscategoriesRepository, $id, Request $request)
+    {
+        $sousCategorie = $souscategoriesRepository->find($id);
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($souscategorie);
+            $manager->flush();
+            $this->addFlash(
+                'success',
+                'Le sous-categorie a bien été modifiée'
+            );
+            return $this->redirectToRoute('admin_sous-categories');
+        }
+        return $this->render('admin/sousCategoriesForm.html.twig', [
+            'sousCategoriesForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/sous-categorie/delete-{id}", name="sous_categorie_delete")
+     */
+    public function deleteSousCategorie(SousCategoriesRepository $souscategoriesRepository, $id)
+    {
+        $sousCategorie = $souscategoriesRepository->find($id);
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($sousCategorie);
+        $manager->flush();
+        $this->addFlash(
+            'success',
+            'Le produit a bien été supprimé'
+        );
+        return $this->redirectToRoute('admin_sous_categories');
     }
 }
