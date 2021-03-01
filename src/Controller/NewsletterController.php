77a7eb3ea269876f415257716cd456d7eb57879b
MatthieuSc <77393056+MatthieuSc@users.noreply.github.com>
2021-03-01 16:03:51 +0100

    mise à jour newsletter

--- src/Controller/NewsletterController.php
+++ src/Controller/NewsletterController.php
@@ -17,23 +17,29 @@ class NewsletterController extends AbstractController
      */
     public function index(Request $request, NewsletterRepository $newsletterRepository, \Swift_Mailer $mailer): Response
     {
-        // $newsletter = new Newsletter();
+        $newsletter = new Newsletter();
 
         $formulaireNewsletter = $this->createForm(NewsletterType::class);
         $formulaireNewsletter->handleRequest($request);
 
         if ($formulaireNewsletter->isSubmitted() && $formulaireNewsletter->isValid()) {
-        $newsletter = $formulaireNewsletter->getData();
+            $newsletter->setEmail($formulaireNewsletter->get('email')->getData());
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($newsletter);
+            $manager->flush();
+        
+            
+
         
 
-        $mail = (new \Swift_Message('Inscription à la Newsletter'))
+            $mail = (new \Swift_Message('Inscription à la Newsletter'))
                 ->setFrom('contact.elibird@gmail.com')
-                ->setTo($newsletter['email'])
+                ->setTo($formulaireNewsletter->get('email')->getData())
                 ->setBody(
                     $this->renderView(
                         'newsletter/newsletterContact.html.twig', [
                             // 'email' => $newsletter->getEmail('email')
-                            'email' => $newsletter['email']
+                            'email' => $formulaireNewsletter->get('email')->getData()
                         ],
                     ),
                     'text/html'
@@ -41,13 +47,12 @@ class NewsletterController extends AbstractController
             ;
             
             $mailer->send($mail);
-            // $em = $this->getDoctrine()->getManager();
-            // $em->persist($newsletter);
-            // $em->flush();
+            
             $this->addFlash(
                 'success',
                 'Votre inscription a bien été prise en compte.'
             );
+            
             
             return $this->redirectToRoute('home');   
         }
